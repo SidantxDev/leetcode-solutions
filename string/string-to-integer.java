d4edc9b0013525b6d75e3f2145355c082f6653e8
SidantxDev <sidantofficial@gmail.com>
2026-08-05 06:03:55 +0530

    Create string-to-integer.java

---
+++ string/string-to-integer.java
@@ -0,0 +1,35 @@
+//8. String to Integer (atoi)
+
+// Skip leading spaces, check the sign, and build the number digit by digit.
+// Use long to detect overflow, then clamp the result to the integer range.
+// Time: O(n) | Space: O(1)
+
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0;
+        int n = s.length();
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+        int sign = 1;
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            if (s.charAt(i) == '-') {
+                sign = -1;
+            }
+            i++;
+        }
+        long ans = 0;
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+            ans = ans * 10 + digit;
+            if (sign == 1 && ans > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            if (sign == -1 && -ans < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+            i++;
+        }
+        return (int)(ans * sign);
+    }
+}
