bf99aa33a895f16e3d1eddb53f2fd642ded4a2c6
SidantxDev <sidantofficial@gmail.com>
2026-08-07 04:33:15 +0530

    Create single_element_in_sorted_array.java

---
+++ binary-search/single_element_in_sorted_array.java
@@ -0,0 +1,32 @@
+//540. Single Element in a Sorted Array
+
+//searching for the odd element from the left side of the array, i.e. before the odd element appeared
+
+//O(N)
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (mid % 2 == 0) {
+                if (nums[mid] == nums[mid + 1]) {
+                    left = mid + 2;
+                } else {
+                    right = mid;
+                }
+            } else {
+                if (nums[mid] == nums[mid - 1]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return nums[left];
+    }
+}
